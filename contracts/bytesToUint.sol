428bc6f539c6db48b750145307bc4b67fcaced65
Drypto | OOKI <51417606+Drypto13@users.noreply.github.com>
2022-05-30 14:55:28 -0700

    Update bytesToUint.sol

--- contracts/bytesToUint.sol
+++ contracts/bytesToUint.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 contract bytesToUint {
     function getValues(bytes calldata iAmounts) public pure returns (uint64[] memory returnValuesOfUint64) {
         returnValuesOfUint64 = new uint64[](iAmounts.length/8);
-        for(uint i=0;i<returnValuesOfUint64.length;){
+        for(uint i=0;i<iAmounts.length/8;){
             returnValuesOfUint64[i] = tt(iAmounts[32*(i/4):32+32*(i/4)])[i%4];
             unchecked {++i;}
         }   
